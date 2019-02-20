8111b931b6630413c5e65aecae86da527c07a888
shuisheng <njshuisheng@126.com>
2019-02-20 15:29:29 +0800

    service

--- src/components/customService/components/menu-list.vue
+++ src/components/customService/components/menu-list.vue
@@ -2,7 +2,7 @@
   <div id="menu-list" v-if="navVisible">
     <div class="menu_list flex-center">
       <div class="menu_contain" v-for="(item,key) in menu_list">
-        <span class="writingMode" :class="['a_delay-' + (key + 1)]" :key="key" @click="handleRouterLink(item.url)">{{ item.title }}</span>
+        <span class="writingMode" :class="{['a_delay-' + (key + 1)] : '...' ,'trans_show' : navVisible  }" :key="key" @click="handleRouterLink(item.url)">{{ item.title }}</span>
       </div>
     </div>
   </div>
@@ -81,6 +81,8 @@
   @mixin a_delay($s) {
     animation-delay: $s + s;
     -webkit-animation-delay: $s + s;
+    transition-delay: $s + s;
+    -webkit-transition-delay: $s + s;
   }
 
   @keyframes show_trans {
@@ -100,13 +102,19 @@
           &:hover {
             @include serviceImg('hongdi.png', 'theme1');
           }
+          opacity: 0;
+          transform: translateX(-1920px);
           transition: all ease 1s;
           animation: show_trans linear .5s;
         }
         @for $i from 1 to 9 {
           .a_delay-#{$i} {
-            @include a_delay($i / 10);
+            @include a_delay((10 - $i ) / 10);
           }
+        }
+        .trans_show {
+          opacity: 1;
+          transform: translateX(0);
         }
       }
     }
