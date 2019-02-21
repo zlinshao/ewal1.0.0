8adeca65496a86f886caf94dcc511b9ebaced20f
shuisheng <njshuisheng@126.com>
2019-02-21 13:59:54 +0800

    work-info

--- src/components/customService/index.vue
+++ src/components/customService/index.vue
@@ -3,154 +3,50 @@
     <div class="container">
       <menu-list :menuVisible="show_menu_list" @close="show_menu_list = false"></menu-list>
     </div>
-    <div class="info_container">
-      <ul class="date_change">
-        <li @click="isActive = tmp.id" v-for="tmp in date_change_list" :key="tmp.id" :class="{ 'mark_li' : isActive === tmp.id }">{{ tmp.val }}</li>
-      </ul>
-      <div class="day_info flex-center">
-        <div class="event_info">
-          <p>事件程度比重：</p>
-          <div id="events_charts" style="width: 400px;height: 300px;"></div>
-        </div>
-        <div class="attend_info">
-          <p>考勤：</p>
-          <div id="attend_charts" style="width: 400px;height: 300px;"></div>
-        </div>
-        <div class="work_info">
-          <p v-for="todo in work_info">{{ todo.work }} : {{ todo.val }}</p>
-        </div>
-      </div>
-    </div>
+    <work-info :work-info="work_info" :event-data="event_data" :attend-data="attend_data" @change="handleChangeDate"></work-info>
   </div>
 </template>
 
 <script>
   import MenuList from './components/menu-list.vue';
+  import WorkInfo from '../common/work-info.vue';
 
   export default {
     name: "index",
-    components: { MenuList },
+    components: { MenuList,WorkInfo },
     data() {
       return {
         show_menu_list: false,
-        date_change_list: [
-          {id: 1,val: '当日'},
-          {id: 2,val: '本周'},
-          {id: 3,val: '本月'},
-        ],
-        isActive: 1,
         work_info: [
           {work: '平均在线时长', val: '8 h'},
           {work: '平均处理用时', val: '30 min'},
           {work: '当日处理事件数', val: '16 件'},
           {work: '本周处理事件数', val: '35 件'},
-        ]
+        ],
+        event_data: [
+          {value: 500, name: '一般'},
+          {value: 300, name: '特殊'},
+          {value: 200, name: '紧急'},
+        ],
+        attend_data: []
       }
     },
     mounted() {
       this.show_menu_list = true;
-      this.init_event_chart();
-      this.init_attend_chart();
+      this.work_info[0].val = '9 h';
+      this.event_data[0].value = 900;
+      this.attend_data = [10,5,2]
     },
     activated() {
     },
     watch: {},
     computed: {},
     methods: {
-      init_event_chart() {
-        var event_chart = this.$echarts.init(document.getElementById('events_charts'));
-        event_chart.setOption({
-          color: ['#DFDFDF','#9E9E9E','#B9292D'],
-          legend: {
-            orient: 'vertical',
-            x: 'left',
-            y: 'middle',
-            data: ['一般', '特殊', '紧急']
-          },
-          series: [
-            {
-              name: '事件紧急程度',
-              type: 'pie',
-              radius: ['30%', '80%'],
-              selectedMode: 'single',
-              avoidLabelOverlap: false,
-              selectedOffset: 15,
-              startAngle: 45,
-              label: {
-                normal: {
-                  position: 'inner',
-                  formatter: '{d}%',
-                  fontFamily: 'jingDianXingShu',
-                  fontSize: 18,
-                  fontStyle: 'italic'
-                },
-                emphasis: {
-                  show: true,
-                  textStyle: {
-                    fontSize: 18,
-                    fontWeight: 'bold'
-                  }
-                }
-              },
-              data: [
-                {value: 500, name: '一般'},
-                {value: 300, name: '特殊'},
-                {value: 200, name: '紧急'},
-              ]
-            }
-          ]
-        });
-      },
-      init_attend_chart() {
-        var attend_chart = this.$echarts.init(document.getElementById('attend_charts'));
-        attend_chart.setOption({
-          color: ['#DFDFDF'],
-          xAxis: [
-            {
-              type: 'category',
-              data: ['迟到','请假','缺勤'],
-              axisTick: {
-                alignWithLabel: true,
-                length: 0
-              },
-              axisLine: {
-                show: false
-              }
-            }
-          ],
-          yAxis: [
-            {
-              type: 'value',
-              show: false
-            }
-          ],
-          label: {
-            show: true,
-            position: 'top',
-            formatter: '{c}',
-            color: '#CF2E33',
-            fontFamily: 'jingDianXingShu',
-            fontSize: 18,
-            fontStyle: 'italic'
-          },
-          emphasis: {
-            itemStyle: {
-              color: '#DFDFDF',
-              barBorderRadius: 5
-            }
-          },
-          barMaxWidth: 25,
-          markLine: false,
-          series: [
-            {
-              name: '考勤',
-              type: 'bar',
-              data: [10,5,2]
-            }
-          ]
-        });
-      },
-    },
+      handleChangeDate(id) {
+        console.log(id);
+        console.log(this.work_info);
+      }
+    }
   }
 </script>
 
