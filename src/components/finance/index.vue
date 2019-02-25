5967cb8b448af56656c1ce2192547daed59f2a08
linshao <601267015@qq.com>
2019-02-25 21:48:20 +0800

    fff

--- src/components/finance/index.vue
+++ src/components/finance/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="finance" class="flex-center">
-    <div class="justify-around" style="padding: 95px 0;">
-      <div class="mainList" v-for="item in 5" style="border:1px solid #CF2E33;width: 25%;"></div>
+    <div class="justify-center">
+      <div class="mainList" v-for="item in 5">
+        <div class="abacusUp">
+          <p><i></i></p>
+        </div>
+        <div class="abacusDown">
+          <p>
+            <span class="writingMode">付款</span>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +43,59 @@
     #finance {
       @include financeImg('ditu.png', 'theme1');
       > div {
+        padding: 95px 0;
         @include financeImg('beijingtu.png', 'theme1');
+      }
+      .mainList {
+        border: 1px solid #CF2E33;
+        width: 100%;
+        padding-right: 5px;
+        &:hover {
+          > div {
+            p {
+              background-color: green;
+              color: $colorFFF;
+            }
+          }
+        }
+        > div {
+          width: 100%;
+          @include flex('flex-center');
+          p {
+            cursor: pointer;
+            @include flex('flex-center');
+            span {
+              @include flex('flex-center');
+            }
+          }
+        }
+        .abacusUp {
+          min-height: 200px;
+          max-height: 200px;
+          p {
+            width: 60px;
+            height: 40px;
+            i {
+              width: 24px;
+              height: 24px;
+              @include financeImg('fukuanicon.png', 'theme1');
+            }
+
+          }
+        }
+        .abacusDown {
+          height: 410px;
+          p {
+            width: 60px;
+            height: 160px;
+            padding-top: 30px;
+            span {
+              font-family: 'jingDianXingShu';
+              letter-spacing: 20px;
+              font-size: 30px;
+            }
+          }
+        }
       }
     }
   }
