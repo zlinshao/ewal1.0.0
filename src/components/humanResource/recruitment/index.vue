e01d6df424453d30fb4a9fa945cce38f91ecfa0d
linshao <601267015@qq.com>
2019-03-01 10:20:18 +0800

    fff

--- src/components/humanResource/recruitment/index.vue
+++ src/components/humanResource/recruitment/index.vue
@@ -2,11 +2,6 @@
   <div id="recruitment">
     <!--<Upload :file="photo1"></Upload>-->
     <!--<Upload :file="photo2"></Upload>-->
-    <el-radio-group v-model="radio2">
-      <el-radio :label="3">备选项</el-radio>
-      <el-radio :label="6">备选项</el-radio>
-      <el-radio :label="9">备选项</el-radio>
-    </el-radio-group>
     <div class="listTopCss items-bet">
       <div class="items-center listTopLeft">
         <p class="flex-center" @click="moduleList">
@@ -51,7 +46,6 @@
     components: {MenuList, SearchHigh, Upload},
     data() {
       return {
-        radio2: '',
         recruitmentSearch,
         humanResource,
         visibleStatus: false,
