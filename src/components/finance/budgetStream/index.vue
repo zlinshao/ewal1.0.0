bea11ab6636c62df9c799c1c2517e4f65bf8195f
shuisheng <njshuisheng@126.com>
2019-03-02 17:09:08 +0800

    feat: go on

--- src/components/finance/budgetStream/index.vue
+++ src/components/finance/budgetStream/index.vue
@@ -6,12 +6,6 @@
           <b>...</b>
         </p>
         <h1>收支流水</h1>
-        <h2 class="items-center">
-          <span v-for="item in selects" @click="changeTabs(item.id)" class="items-column"
-                :class="{'chooseTab': chooseTab === item.id}">
-            {{item.title}}<i></i>
-          </span>
-        </h2>
       </div>
       <div class="items-center listTopRight">
         <div class="icons bank" @click="handleOpenBankRun"></div>
@@ -31,17 +25,25 @@
           align="center"
           label="紧急程度">
           <template slot-scope="scope">
-            <div class="status" :class="['status' + scope.row.status]">
-              <p>{{status[scope.row.status]}}</p>
+            <div class="status" :class="['status' + scope.row.scope]">
+              <p>{{status[scope.row.scope]}}</p>
             </div>
           </template>
         </el-table-column>
-        <el-table-column
-          v-for="item in Object.keys(showData)" :key="item"
-          align="center"
-          :prop="item"
-          :label="showData[item]">
-        </el-table-column>
+        <el-table-column label="交易时间" prop="operate_time" min-width="150px" align="center"></el-table-column>
+        <el-table-column label="客户姓名" prop="customer.customer_name" align="center"></el-table-column>
+        <el-table-column min-width="180px" label="地址" prop="address" align="center"></el-table-column>
+        <el-table-column label="科目名称" prop="subject" align="center"></el-table-column>
+        <el-table-column label="类型" prop="cate" align="center"></el-table-column>
+        <el-table-column min-width="130px" label="账户名称" prop="name" align="center"></el-table-column>
+        <el-table-column min-width="150px" label="卡号" prop="account_num" align="center"></el-table-column>
+        <el-table-column label="应收金额" prop="amount_receivable" align="center"></el-table-column>
+        <el-table-column label="实收金额" prop="amount_received" align="center"></el-table-column>
+        <el-table-column label="应付金额" prop="amount_payable" align="center"></el-table-column>
+        <el-table-column label="实付金额" prop="amount_paid" align="center"></el-table-column>
+        <el-table-column label="账户余额" prop="amount_remain" align="center"></el-table-column>
+        <el-table-column label="详细信息" prop="info" min-width="300px" align="center"></el-table-column>
+        <el-table-column label="收/付款人员" prop="operator.name" align="center"></el-table-column>
       </el-table>
       <footer class="flex-center bottomPage">
         <div class="develop flex-center">
@@ -57,17 +59,19 @@
     </div>
     <FinMenuList :module="showFinMenuList" @close="showFinMenuList = false"></FinMenuList>
     <SearchHigh :module="showSearch" :showData="searchData" @close="hiddenModule"></SearchHigh>
+
+    <!--银行流水-->
     <lj-dialog
       :visible="bank_run_visible"
       :size="bank_run_size"
       @close="bank_run_visible = false"
     >
       <div class="dialog_container">
-        <div class="dialog_header flex">
+        <div class="dialog_header flex-center">
           <h3>银行流水</h3>
           <div>
-            <div class="icons"><b>+</b></div>
-            <div class="icons search"></div>
+            <span class="bank"></span>
+            <span class="search"></span>
           </div>
         </div>
         <div class="dialog_main">
@@ -98,6 +102,7 @@
         </div>
       </div>
     </lj-dialog>
+
   </div>
 </template>
 
@@ -121,229 +126,29 @@
           {time: '2019-01-01 15:12',ci: '4627894612847682746',num: 122,run_num: 1212,operator: '冯宝宝'},
           {time: '2019-01-01 15:12',ci: '4627894612847682746',num: 122,run_num: 1212,operator: '冯宝宝'},
         ],
-        showFinMenuList: false,
-        chooseTab: 1,
-        selects: [
-          {
-            id: 1,
-            title: '流水',
-          },
-          {
-            id: 2,
-            title: '回滚',
-          },
-          {
-            id: 3,
-            title: '归档',
-          }
-        ],
 
-        tableData: [
-          {
-            id: 10,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 20,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 30,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 11,
-            status: 2,
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄',
-          },
-          {
-            id: 12,
-            status: 1,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 13,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 23,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 33,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 10,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 20,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 30,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 11,
-            status: 2,
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄',
-          },
-          {
-            id: 12,
-            status: 1,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 13,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 10,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 20,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 30,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 11,
-            status: 2,
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄',
-          },
-          {
-            id: 12,
-            status: 1,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 13,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 23,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 33,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 10,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 20,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 30,
-            status: 1,
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄',
-          },
-          {
-            id: 11,
-            status: 2,
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄',
-          },
-          {
-            id: 12,
-            status: 1,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-          {
-            id: 13,
-            status: 2,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-          },
-        ],
-        status: {
-          1: '收入',
-          2: '支出',
+
+        showFinMenuList: false,
+
+        params: {
+          search: '',
+          page: 1,
+          limit: 12,
+          start_date: '',
+          end_date: '',
+          cate: '',
+          subject_id: '',
+          account_id: '',
+          operate_start_date: '',
+          operate_end_date: '',
+          export: '',
         },
-        showData: {
-          date: '日期',
-          name: '姓名',
-          address: '地址',
+        tableData: [],
+        tableCount: 0,
+
+        status: {
+          3: '收入',
+          5: '支出',
         },
         chooseRowIds: [],
 
@@ -355,12 +160,28 @@
       }
     },
     mounted() {
+      this.getAccountRunList();
     },
     activated() {
     },
     watch: {},
     computed: {},
     methods: {
+      getAccountRunList() {
+        this.$http.get(globalConfig.temporary_server + 'fund_flow_record',this.params).then(res => {
+          console.log(res);
+          if (res.code === 200) {
+            this.tableData = res.data.data;
+            this.tableCount = res.data.count;
+          } else {
+            this.tableData = [];
+            this.tableCount = 0;
+          }
+        }).catch(err => {
+          console.log(err);
+        })
+      },
+
       handleOpenBankRun() {
         this.bank_run_size = 'large';
         this.bank_run_visible = true;
@@ -506,14 +327,35 @@
         .status {
           color: $colorFFF;
         }
-        .status1 {
+        .status3 {
           p {
             @include budgetStreamImg('shouru.png', 'theme1');
           }
         }
-        .status2 {
+        .status5 {
           p {
             @include budgetStreamImg('zhichu.png', 'theme1');
+          }
+        }
+      }
+      .dialog_header {
+        h3,div {
+          width: 100%;
+        }
+        > div {
+          text-align: right;
+          > span {
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            @include radius(50%);
+            cursor: pointer;
+          }
+          .bank {
+            background-color: #00FDFF;
+          }
+          .search {
+            background-color: #008A7B;
           }
         }
       }
