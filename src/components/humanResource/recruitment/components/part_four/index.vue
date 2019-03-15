87330a2451baa31b1f60da6bd8184b9cbb3609b1
shuisheng <njshuisheng@126.com>
2019-03-15 17:50:17 +0800

    to be continued

---
+++ src/components/humanResource/recruitment/components/part_four/index.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id="part_four">
+      <div class="flex-center">
+        <div class="container flex-center">
+          <div class="content">
+            <el-table
+              :data="tableList"
+              highlight-current-row
+              :row-class-name="tableChooseRow"
+              @cell-click="tableClickRow"
+              header-row-class-name="tableHeader"
+              style="width: 100%"
+            >
+              <el-table-column label="岗位" prop="position" align="center"></el-table-column>
+              <el-table-column label="姓名" prop="name" align="center"></el-table-column>
+              <el-table-column label="来源" prop="come" align="center"></el-table-column>
+              <el-table-column label="入职资料反馈">
+                <template slot-scope="scope">
+                  <el-button size="mini" type="success" plain>资料未补齐</el-button>
+                </template>
+              </el-table-column>
+              <el-table-column label="入职结果">
+                <template slot-scope="scope">
+                  <el-button size="mini" type="warning" plain>待确定</el-button>
+                </template>
+              </el-table-column>
+              <el-table-column label="入职通知">
+                <template slot-scope="scope">
+                  <el-button size="mini" type="primary" plain>发送offer</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "",
+        data() {
+            return {
+              tableList: [
+                {name: '冯宝宝',come: '智联',position: 'web前端工程师'},
+                {name: '冯宝宝',come: '智联',position: '客服专员'},
+                {name: '冯宝宝',come: '智联',position: 'PHP工程师'},
+                {name: '冯宝宝',come: '智联',position: '客服专员'},
+                {name: '冯宝宝',come: '智联',position: '客服专员'},
+                {name: '冯宝宝',come: '智联',position: '客服专员'},
+              ],
+              chooseRowIds: [], //图标点击
+            }
+        },
+        mounted() {
+        },
+        activated() {
+        },
+        watch: {},
+        computed: {},
+        methods: {
+          // 当前点击
+          tableClickRow(row) {
+            let ids = this.chooseRowIds;
+            ids.push(row.id);
+            this.chooseRowIds = this.myUtils.arrayWeight(ids);
+          },
+          // 点击过
+          tableChooseRow({row, rowIndex}) {
+            return this.chooseRowIds.includes(row.id) ? 'tableChooseRow' : '';
+          },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../../../assets/scss/humanResource/recruitment/components/part_four.scss";
+  @mixin part_three_img($m,$n) {
+    $url: '../../../../../assets/image/humanResource/recruitment/' + $n + '/' + $m;
+    @include bgImage($url);
+  }
+
+  #theme_name {
+    #part_four {
+      > div {
+        .container {
+          @include part_three_img('ksszbj.png','theme1');
+        }
+      }
+    }
+  }
+</style>
--- src/components/humanResource/recruitment/index.vue
+++ src/components/humanResource/recruitment/index.vue
@@ -1,16 +1,77 @@
 <template>
   <div id="recruitment">
     <div>
-      recruitment
+      <div class="listTopCss items-bet">
+        <div class="items-center listTopLeft">
+          <p class="flex-center">
+            <b>...</b>
+          </p>
+          <h1>招兵买马</h1>
+          <h2 class="items-center">
+          <span v-for="item in selects" @click="changeTabs(item.id)" class="items-column"
+                :class="{'chooseTab': chooseTab === item.id}">
+            {{item.title}}<i></i>
+          </span>
+          </h2>
+        </div>
+        <div class="items-center listTopRight">
+          <el-button size="mini" type="warning" plain v-if="chooseTab === 2" @click="ms_add_visible = true">添加面试人</el-button>
+          <el-button size="mini" type="success" plain v-if="chooseTab === 2" style="margin-right: 10px">添加面试官</el-button>
+          <div class="icons add" v-if="chooseTab === 1" @click="mb_add_visible = true"><b>+</b></div>
+          <div class="icons search" @click="showSearch = true"></div>
+        </div>
+      </div>
+      <div class="recruitment_main">
+        <part-one
+          v-if="chooseTab === 1"
+          :add-modules="mb_add_visible"
+          @close="mb_add_visible = false"
+        ></part-one>
+        <part-two
+          v-if="chooseTab === 2"
+          :add-interviewer-visible="ms_add_visible"
+          @closeMs="ms_add_visible = false"
+        ></part-two>
+        <part-three v-if="chooseTab === 3"></part-three>
+        <part-four v-if="chooseTab === 4"></part-four>
+      </div>
+      <!--高级搜索-->
+      <SearchHigh :module="showSearch" :showData="searchData" @close="hiddenModule"></SearchHigh>
     </div>
   </div>
 </template>
 
 <script>
+  import SearchHigh from '../../common/searchHigh.vue';
+  import PartOne from './components/part_one/index.vue'; //募兵行列
+  import PartTwo from './components/part_two/index.vue'; //分取科士
+  import PartThree from './components/part_three/index.vue'; //殿试会师
+  import PartFour from './components/part_four/index.vue'; //榜上有名
+
   export default {
     name: "index",
+    components: { SearchHigh,PartOne,PartTwo,PartThree,PartFour },
     data() {
-      return {}
+      return {
+        //导航
+        selects: [
+          {id: 1, title: '募兵行列'},
+          {id: 2, title: '分取科士'},
+          {id: 3, title: '殿试会师'},
+          {id: 4, title: '榜上有名'}
+        ], //模块列表
+        chooseTab: 2, //当前选中模块
+
+        //搜索
+        showSearch: false,
+        searchData: [],
+
+        //募兵行列
+        mb_add_visible: false,
+
+        //添加面试人
+        ms_add_visible: false
+      }
     },
     mounted() {
     },
@@ -18,7 +79,17 @@
     },
     watch: {},
     computed: {},
-    methods: {},
+    methods: {
+      // tab切换
+      changeTabs(id) {
+        this.chooseTab = id;
+        this.$store.dispatch('route_animation');
+      },
+      //关闭搜索
+      hiddenModule() {
+        this.showSearch = false;
+      },
+    },
   }
 </script>
 
@@ -32,7 +103,14 @@
 
   #theme_name.theme1 {
     #recruitment {
-
+      height: 100%;
+      > div {
+        height: 100%;
+        .recruitment_main {
+          width: 100%;
+          height: calc(100% - 90px);
+        }
+      }
     }
   }
 
