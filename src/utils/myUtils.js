975ebfc8d0f6c58343c11e604829188e468836ec
tianjian123456 <1149375181@qq.com>
2019-04-02 21:03:15 +0800

    'adf'

--- src/utils/myUtils.js
+++ src/utils/myUtils.js
@@ -38,6 +38,35 @@ class myUtils {
     }
     return temp;
   }
+  //格式化日期
+  static formatDate(v, format='yyyy-MM-dd') {
+    if (!v) return null;
+    let d = v;
+    if (typeof v === 'string') {
+      if (v.indexOf("/Date(") > -1)
+        d = new Date(parseInt(v.replace("/Date(", "").replace(")/", ""), 10));
+      else
+        d = new Date(Date.parse(v.replace(/-/g, "/").replace("T", " ").split(".")[0]));//.split(".")[0] 用来处理出现毫秒的情况，截取掉.xxx，否则会出错
+    }
+    let o = {
+      "M+": d.getMonth() + 1,  //month
+      "d+": d.getDate(),       //day
+      "h+": d.getHours(),      //hour
+      "m+": d.getMinutes(),    //minute
+      "s+": d.getSeconds(),    //second
+      "q+": Math.floor((d.getMonth() + 3) / 3),  //quarter
+      "S": d.getMilliseconds() //millisecond
+    };
+    if (/(y+)/.test(format)) {
+      format = format.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (let k in o) {
+      if (new RegExp("(" + k + ")").test(format)) {
+        format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+      }
+    }
+    return format;
+  }
 }
 
 export default myUtils
