e40400f8c21cc32d96036b5aa10fe152d919628e
tianjian123456 <1149375181@qq.com>
2019-04-10 17:38:56 +0800

    'adf';

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
             <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552912676050&di=fd46be51272d18ea8ffc89e2956a8d4c&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F8d64400852949b685670d52be88910a57e2e1542.jpg">
           </p>
 
-            <span  class="icon3024 icon-personal-center"></span>
+            <span @click="routerLink('personalCenter')" class="icon3024 icon-personal-center"></span>
         </div>
       </div>
     </header>
---
+++ src/components/personalCenter/index.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="personal_center">
+      <div class="personal-center-container">
+        <div class="container-left">
+          <div class="container-left-up">
+            <div class="personal-info">
+              <div class="personal-info-photo"></div>
+              <div class="personal-info-name">
+                张三
+                <div class="personal-info-badge">P2</div>
+              </div>
+              <div class="personal-info-status">
+                的说法十分艰苦拉萨sadSadfas sadf
+              </div>
+            </div>
+            <div class="personal-group-button"></div>
+          </div>
+          <div class="container-left-down"></div>
+        </div>
+        <div class="container-right">
+          <div class="calendar"></div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/scss/personalCenter/index.scss';
+
+  @mixin personalCenterImg($m, $n) {
+    $url: '../../assets/image/personalCenter/' + $n + '/' + $m;
+    @include bgImage($url);
+  }
+  #theme_name {
+    #personalCenter {
+
+    }
+  }
+
+  #theme_name.theme1 {
+    #personal_center {
+      .personal-center-container {
+        .container-left {
+          .personal-info-badge {
+            @include personalCenterImg('fill_1.png','theme1');
+          }
+        }
+      }
+    }
+  }
+
+  #theme_name.theme2 {
+    #personal_center {
+
+    }
+  }
+
+  #theme_name.theme3 {
+    #personal_center {
+
+    }
+  }
+
+  #theme_name.theme4 {
+    #personal_center {
+
+    }
+  }
+</style>
