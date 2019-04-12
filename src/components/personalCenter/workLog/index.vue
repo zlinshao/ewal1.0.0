da585721ddc815b1c6c132d20219de8b8cef4b09
tianjian123456 <1149375181@qq.com>
2019-04-12 16:25:53 +0800

    'sdf';

--- src/components/personalCenter/workLog/index.vue
+++ src/components/personalCenter/workLog/index.vue
@@ -3,13 +3,13 @@
     <div class="work-log-container">
       <div class="work-log-container-left scroll_bar">
         <div class="work-log-toolbar">
-          <div v-for="item in toolbarList" @click="toolbarChoosed=item.id" :class="{checked:toolbarChoosed==item.id}"
+          <div v-for="item in toolbarList" @click="switchToolbar(item.id)" :class="{checked:toolbarChoosed==item.id}"
                class="work-log-toolbar-item">{{item.name}}
           </div>
         </div>
         <div class="work-log-input-list scroll_bar">
           <div class="work-log-input-list-container">
-            <div v-for="item in inputList" class="work-log-input-list-item">
+            <div v-for="item in inputList[inputIndexName]" class="work-log-input-list-item">
               <div class="work-log-input-list-item-tip">
                 {{item.name}}
               </div>
@@ -24,10 +24,12 @@
             </div>
             <div class="work-log-input-list-item-footer">
               <div class="footer-upload">
-                上传附件:<lj-upload style="display: inline-block;margin-left: 30px"></lj-upload>
+                上传附件:
+                <lj-upload style="display: inline-block;margin-left: 30px"></lj-upload>
               </div>
               <div class="footer-user-choose">
-                通知人:<user-choose title="请选择发送人员" style="margin-left: 30px"></user-choose>
+                通知人:
+                <user-choose title="请选择发送人员" style="margin-left: 30px"></user-choose>
               </div>
 
               <div class="footer-button">
@@ -39,7 +41,22 @@
           </div>
         </div>
       </div>
-      <div class="work-log-container-right"></div>
+      <div class="work-log-container-right">
+        <div class="timeline-container">
+          <el-timeline>
+            <el-timeline-item timestamp="2018/4/12 12:24:52" placement="top">
+              <h3>今日完成工作:</h3>
+              <p>由张晓莲（员工关系组-基础人事专员）为您办理了入职手续，入职部门：研发中心</p>
+            </el-timeline-item>
+            <el-timeline-item timestamp="2018/4/3" placement="top">
+              <p>由张晓莲（员工关系组-基础人事专员）为您办理了入职办理了入职办理了入职办理了入职手续，入职部门：研发中心，入职岗位：测试，等级：实习</p>
+            </el-timeline-item>
+            <el-timeline-item timestamp="2018/4/2" placement="top">
+              <p>由张晓莲（员工关系组-基础人事专员）为您办理了入职手续，入职部门：研发中心</p>
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -77,34 +94,175 @@
             name: '业绩日报'
           },
         ],
-        textarea:'',
+        textarea: '',
 
-        inputList:[
-          {
-            id:1,
-            name:'今日完成工作',
-            textarea:'',
-          },
-          {
-            id:2,
-            name:'未完成工作',
-            textarea:'',
-          },
-          {
-            id:3,
-            name:'需协调工作',
-            textarea:'',
-          },
-          {
-            id:4,
-            name:'备注',
-            textarea:'',
-          },
-        ],
+        inputIndexName: 'daily',
+        inputList: {
+          daily: [
+            {
+              id: 1,
+              name: '今日完成工作',
+              textarea: '',
+            },
+            {
+              id: 2,
+              name: '未完成工作',
+              textarea: '',
+            },
+            {
+              id: 3,
+              name: '需协调工作',
+              textarea: '',
+            },
+            {
+              id: 4,
+              name: '备注',
+              textarea: '',
+            },
+          ],
+          weekly: [
+            {
+              id: 1,
+              name: '本周完成工作',
+              textarea: '',
+            },
+            {
+              id: 2,
+              name: '本周工作总结',
+              textarea: '',
+            },
+            {
+              id: 3,
+              name: '下周工作计划',
+              textarea: '',
+            },
+            {
+              id: 4,
+              name: '需协调与帮助',
+              textarea: '',
+            },
+            {
+              id: 5,
+              name: '备注',
+              textarea: '',
+            },
+          ],
+          monthly: [
+            {
+              id: 1,
+              name: '本月完成工作',
+              textarea: '',
+            },
+            {
+              id: 2,
+              name: '本月工作总结',
+              textarea: '',
+            },
+            {
+              id: 3,
+              name: '下月工作计划',
+              textarea: '',
+            },
+            {
+              id: 4,
+              name: '需协调与帮助',
+              textarea: '',
+            },
+            {
+              id: 5,
+              name: '备注',
+              textarea: '',
+            },
+          ],
+          pfeDaily: [//业绩日报
+            {
+              id: 1,
+              name: '今日营业额',
+              textarea: '',
+            },
+            {
+              id: 2,
+              name: '今日客户数',
+              textarea: '',
+            },
+            {
+              id: 3,
+              name: '月累计营业额',
+              textarea: '',
+            },
+            {
+              id: 4,
+              name: '本月业绩目标',
+              textarea: '',
+            },
+            {
+              id: 5,
+              name: '今日思考',
+              textarea: '',
+            },
+            {
+              id: 6,
+              name: '备注',
+              textarea: '',
+            },
+          ]
+        },
+      }
+    },
+    methods: {
+      switchToolbar(index) {
+        this.toolbarChoosed = index;
+        switch (index) {
+          case 1:
+            this.inputIndexName = 'daily';
+            break;
+          case 2:
+            this.inputIndexName = 'weekly';
+            break;
+          case 3:
+            this.inputIndexName = 'monthly';
+            break;
+          case 4:
+            this.inputIndexName = 'pfeDaily';
+            break;
+        }
       }
     },
   }
 </script>
+
+
+<style lang="scss">
+  #work_log {
+    .timeline-container {
+      .el-timeline-item__wrapper {
+        text-align: left;
+        .el-timeline-item__timestamp {
+          display: flex;
+          justify-content: flex-start;
+          align-items: flex-start;
+          height: 35px;
+          //line-height: 35px;
+          font-size: 16px;
+          //color: #CF2E33;
+          color: #686874;
+          font-family: MicrosoftYaHei-Bold;
+          font-weight: 700;
+        }
+        h3 {
+          font-family: MicrosoftYaHei-Bold;
+          font-weight: bold;
+          height: 26px;
+          line-height: 26px;
+          font-size: 14px;
+          color: #686874;
+        }
+      }
+    }
+  }
+
+</style>
+
 
 <style scoped lang="scss">
   @import "../../../assets/scss/personalCenter/workLog/index";
