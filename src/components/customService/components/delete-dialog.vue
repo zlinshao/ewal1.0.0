fa4e3fa99c011d165c9e7715f70038a27ea9ee26
77chenmei <1219123218@qq.com>
2019-04-18 15:46:00 +0800

    ff

--- src/components/customService/components/delete-dialog.vue
+++ src/components/customService/components/delete-dialog.vue
@@ -7,7 +7,7 @@
         <p class='notice'>确定要删除这条工单吗?</p>
       </div>
       <div class="dialog_footer">
-        <el-button type="danger" size="small" @click="handleCloseAddOrder">新增</el-button>
+        <el-button type="danger" size="small" @click="handleCloseAddOrder">删除</el-button>
         <el-button type="info" size="small" @click="handleCloseDelete">取消</el-button>
       </div>
     </div>
