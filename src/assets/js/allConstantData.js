e01d6dc6c7ecb9619d8d6293056c76f2d7dd67a6
tianjian123456 <1149375181@qq.com>
2019-05-09 17:56:11 +0800

    'sf';

--- src/assets/js/allConstantData.js
+++ src/assets/js/allConstantData.js
@@ -6,7 +6,7 @@ const DROPDOWN_CONSTANT = {//下拉列表常量
     },//添加物品
     GOODS_DETAIL: {
       GOODS_STATUS:[{id:0,name:'无'}, {id:1,name:'待维修'}, {id:2,name:'维修中'}, {id:3,name:'已维修'}, {id:4,name:'报废'}],//物品状态
-      RECEIVE_RETURN_STATUS:[{id:0,name:'待通知'}, {id:1,name:'待领取'}, {id:2,name:'部分已领取'}, {id:3,name:'已领取'}, {id:4,name:'放弃领取'}, {id:5,name:'待归还'}, {id:6,name:'部分已归还'}, {id:7,name:'已归还'}],//领还状态
+      RECEIVE_RETURN_STATUS:[{id:0,name:'待通知'}, {id:1,name:'待领取'}, {id:2,name:'已领取'}, {id:3,name:'放弃领取'}, {id:4,name:'待归还'}, {id:5,name:'已归还'}],//领还状态
       RESPONSIBLE:[{id:1,name:'个人'},{id:2,name:'部门'},{id:3,name:'公司'}],//任责类型
       PAYMENT: [{id:1,name:'工资扣款'},{id:2,name:'付现'}],//付款方式
     },//物品详情
