10dafb9425d6fc1b327059f7520280b562a3074a
wangxuan <1345085349@qq.com>
2019-06-28 16:02:42 +0800

    bug修复

--- src/components/customService/housingDue/index.vue
+++ src/components/customService/housingDue/index.vue
@@ -407,13 +407,32 @@ export default {
       } else {
         _.forEach(data.data,(item)=>{
            if(item.keyName=='tag_status'){
-            item.keyType= 'check',
-            item.title ='状态';
+             item.keyType= 'check',
+              item.title ='状态';
+             if(this.chooseTab===1){
+                item.value =[{
+                    id: 1,
+                    title: '续约'
+                },
+                {
+                    id: 2,
+                    title: '退房'
+                }]
+             } else {
+                item.value =[{
+                    id: 1,
+                    title: '续租'
+                },
+                {
+                    id: 2,
+                    title: '退租'
+                }]
+             }
            }
         });
       }
       this.searchData=data;
-      console.log('this.searchData-----', this.searchData);
+      // console.log('this.searchData-----', this.searchData);
     },
     hiddenModule (val) {
       this.showSearch = false;
